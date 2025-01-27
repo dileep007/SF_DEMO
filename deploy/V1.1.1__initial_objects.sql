8d496c8efe58db17ca3780e539ee56f93e6cc647
dileep007 <34852272+dileep007@users.noreply.github.com>
2025-01-27 13:00:27 +0530

    Update V1.1.1__initial_objects.sql

--- deploy/V1.1.1__initial_objects.sql
+++ deploy/V1.1.1__initial_objects.sql
@@ -13,16 +13,16 @@ CREATE TABLE IF NOT EXISTS hrdata.employees
    , commission_pct NUMBER(2,2)
    , manager_id NUMBER(6)
    , dept_id NUMBER(4)
-   , PRIMARY KEY (employee_id)
-   ) ; 
+   );
+  
 
 CREATE TABLE IF NOT EXISTS hrdata.departments
    ( dept_id NUMBER(4)
    , department_name VARCHAR(30) NOT NULL
    , manager_id NUMBER(6)
    , location_id NUMBER(4)
-   ,PRIMARY KEY (dept_id)
-   ) ;
+   );
+ 
 
 -- Insert data into tables
 INSERT INTO hrdata.employees VALUES 
