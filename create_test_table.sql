df660337c8a0b33d027bb3a2c57150d20387eaec
dileep007 <34852272+dileep007@users.noreply.github.com>
2025-01-28 13:33:37 +0530

    Create create_test_table.sql

---
+++ create_test_table.sql
@@ -0,0 +1,10 @@
+--liquibase formatted sql
+
+-- Changeset id:3 author:dev
+CREATE TABLE TEST_TABLE (
+    DEMO_ID NUMBER,
+    DESCRIPTION VARCHAR(255) NOT NULL,
+    CREATED_AT TIMESTAMP NOT 
+);
+
+--rollback DROP TABLE DEMO_TABLE;
