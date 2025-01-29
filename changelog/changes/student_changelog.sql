f3f5318dd6146aae80c23290b9bda0ce970694ef
dileep007 <34852272+dileep007@users.noreply.github.com>
2025-01-29 18:48:40 +0530

    Update student_changelog.sql

--- changelog/changes/student_changelog.sql
+++ changelog/changes/student_changelog.sql
@@ -9,6 +9,16 @@ CREATE TABLE student (
     email VARCHAR(100)
 );
 
+--rollback drop table student;
+
 -- changeset your_name:1002
 INSERT INTO student (student_id, first_name, last_name, age, email)
 VALUES (1, 'John', 'Doe', 20, 'john.doe@example.com');
+
+--rollback CREATE TABLE student (
+    student_id INT,
+    first_name VARCHAR(50),
+    last_name VARCHAR(50),
+    age INT,
+    email VARCHAR(100)
+);
