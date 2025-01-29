a7cbfb9f01e53ce8e1366a6858fa4f96b03ed444
dileep007 <34852272+dileep007@users.noreply.github.com>
2025-01-29 14:48:20 +0530

    Update tests.sql

--- changelog/changes/tests.sql
+++ changelog/changes/tests.sql
@@ -1,11 +1,11 @@
 -- changeset your_name:10
 CREATE TABLE first_table (
-    id INT PRIMARY KEY,
+    id INT,
     name VARCHAR(255)
 );
 
 -- changeset your_name:11
 CREATE TABLE second_table (
-    id INT PRIMARY KEY,
+    id INT,
     description VARCHAR(255)
 );
